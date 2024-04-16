98850a07a7d7b27374d68235f63158a3baa26d3d
Hulrik <166620034+Hulrik@users.noreply.github.com>
2024-04-16 11:50:28 -0300

    Add files via upload

---
+++ pasta senac/Senac-Entregas-main/aula-06-exercicios/exercicio-1/index.js
@@ -0,0 +1,10 @@
+let nome
+let idade
+console.log ("essa variavel declara o nome",typeof nome)
+console.log ("essa variavel declara a idade",typeof idade)/*foi expresso desse jeito porque ainda não foi colocado um valor*/ 
+nome = prompt ("qual o seu nome?")
+idade = prompt ("qual a sua idade")
+console.log ("essa variavel declara o nome",typeof nome)
+console.log ("essa variavel declara a idade",typeof idade) /* agora apareceu a palavra string ao lado da frase*/
+ console.log ("olá", nome, "você tem", idade, "anos")
+ 
---
+++ pasta senac/Senac-Entregas-main/aula-06-exercicios/exercicio-2/index.js
@@ -0,0 +1,4 @@
+let cor = prompt ("você tem cor de olho azul?")
+let altura = prompt ("você tem 1,80 de altura?")
+let peso = prompt ("você pesa 65kg?")
+console.log ("voeê tem a cor do olho azul?",cor, "você tem 1,80 de altura?",altura, "você pesa 65kg?",peso)
