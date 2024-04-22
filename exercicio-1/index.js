160856836233ae6f485b0e20768da9d123e8b634
Hulrik <166620034+Hulrik@users.noreply.github.com>
2024-04-22 11:49:52 -0300

    aula 8 exercicios

---
+++ exercicio-1/index.js
@@ -0,0 +1,96 @@
+/*let idade= Number (prompt ("qual a sua idade?"))
+let amigo= Number (prompt("qual a idade do seu melhor amigo?"))
+let maior = idade > amigo 
+console.log ("sua idade é maior doque seu melhor amigo?",maior)
+let diferença=  idade - amigo
+console.log  (diferença) */
+
+////////////////////////////////////////////////////////
+
+/*let nome = "alex"
+let cor = "Azul"
+let frase = `meu nome é ${nome} e minha cor favorita é ${cor}`
+console.log (frase) */
+
+/*let atring = ´teste, + let, `teste2, + let2 */
+
+//////////////////////////////////////////////////////
+
+/*let nome = ("Hulrik".length)
+console.log (nome)
+
+ let nome2 = "juana".toLowerCase()
+ let nome3 = "juana".toUpperCase()
+ console.log (nome2,nome3)  */
+
+//////////////////////////////////////////////////////
+
+/*let nome = "tetando"
+let semespaço = nome.trim() */
+
+/////////////////////////////////////////////////////
+
+/*let frase = "hoje eu pintei um quadro"
+let frasegrande = frase.Length
+let maiusculo = frase.toUpperCase()
+let io = frase.replaceAll("i","o")
+console.log (frase, frasegrande,maiusculo,io) */
+
+
+/*exemplo do professor: */
+
+/*let frase = prompt  ("escreva uma frase")
+let frasetamanho = frase.length
+let frasemaiuscula = frase.toUpperCase()
+let frase1 = frasemaiuscula.replaceAll("O","I")
+
+console.log ("frase original:",frase)
+console.log ("tamanho da frase:",frasetamanho)
+console.log ("frase em letras maiusculas:",frasemaiuscula)
+console.log ("frase em lingua do I:",frase1) */
+
+////////////////////////////////////////////////////
+
+/*  exemplo:  */
+
+/* let listadecompra = ["batata","cenoura","maça"]
+let numeros = [33,45,36,37,17,97]
+console.log (listadecompra)
+console.log (numeros) */
+//
+/*let listacompra = ["banana","uva","maça"]
+console.log (listadecompra[0])
+console.log (listadecompra[2]) */
+//////////////////////////////////////////////////////
+
+/*let raça = ["pitbull","salsicha","pintcher","pastor alemão","hotvailer"]
+let numero = Number (prompt("insira um numero de 0 a 4:"))
+console.log (raça,numero) */
+
+///////////////////////////////////////////////////////
+/*
+let fruta = ["ovo","milho","leite"]
+fruta.push ("nescau","açucar")
+let maça = fruta.includes("maça")
+let ultimafruta = fruta.pop()
+console.log (fruta)
+... */
+//////////////////////////////////////////////////////////
+
+/*
+
+let numeros = [1, 2, 3, 4, 5, 6]
+let tamanho = numeros.length
+
+console.log ("tamanho da array:",tamanho)
+
+let sete = numeros.push(7)
+let splice1 = numeros.splice(3,2)
+
+console.log ("numeros array",numeros)
+console.log ("numero adicionado:",sete)
+console.log ("numeros removidos:",splice1) 
+*/
+
+//////////////////////////////////////////////////////////////
+
